32f8a8c58acb3b68c68bb8a7f9169fd6cfae14c7
adryanynfante13 <adryanynfante@gmail.com>
2021-10-29 18:15:22 -0500

    inicio jQuery
    jQuery

--- js/funciones.js
+++ js/funciones.js
@@ -3,6 +3,15 @@ jQuery(document).ready(listo);
 
 
 function listo(){
-    alert("Hola mundo"); 
+    jQuery(".hamb").click(function(e){
+        e.preventDefault(); 
+        jQuery("header .container  nav").toggleClass("open");
+        jQuery(".hamb i").toggleClass("fa-times");
+    
+        
+    });
+    jQuery("header .container  nav a").click(function(){
+        var dev = jQuery(this).attr("href");
+    })
 
 }
