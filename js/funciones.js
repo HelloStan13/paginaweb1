dbf4150c0b2ecc910b5bd61983e5bb78c3d52dac
adryanynfante13 <adryanynfante@gmail.com>
2021-10-30 16:27:59 -0500

    jQuery
    function finalizado jQuery

--- js/funciones.js
+++ js/funciones.js
@@ -2,13 +2,12 @@ jQuery(document).ready(listo);
 
 
 
-function listo(){
+function listo()
+{
     jQuery(".hamb").click(function(e){
         e.preventDefault(); 
         jQuery("header .container  nav").toggleClass("open");
-        jQuery(".hamb i").toggleClass("fa-times");
-    
-        
+        jQuery(".hamb i").toggleClass("fa-times");  
     });
     jQuery("header .container  nav a").click(function(){
         jQuery("header .container  nav").removeClass("open");
